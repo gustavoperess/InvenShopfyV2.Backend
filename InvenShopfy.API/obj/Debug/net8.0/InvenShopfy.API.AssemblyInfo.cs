83c32c87e4f37a8304e61ce0b1ce9bbe8345cb3d
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-04 15:22:49 +0100

    adding changes

--- InvenShopfy.API/obj/Debug/net8.0/InvenShopfy.API.AssemblyInfo.cs
+++ InvenShopfy.API/obj/Debug/net8.0/InvenShopfy.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a58ee1c1d61a50b53e65ae190e5fb7fc2025ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ InvenShopfy.Core/Requests/Request.cs
@@ -0,0 +1,6 @@
+namespace InvenShopfy.Core.Requests;
+
+public class Request
+{
+    public string UserId { get; set; } = string.Empty;
+}
