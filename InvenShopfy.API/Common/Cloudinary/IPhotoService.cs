4ccd576c2815c0acdc138ced3fa87c21389e0f64
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-05 16:51:18 +0100

    adding cloudinary to files

---
+++ InvenShopfy.API/Common/Cloudinary/IPhotoService.cs
@@ -0,0 +1,8 @@
+namespace InvenShopfy.API.Common.Cloudinary;
+using CloudinaryDotNet.Actions;
+using System.Threading.Tasks;
+
+public interface IPhotoService
+{
+    Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+}
--- InvenShopfy.API/obj/Debug/net8.0/InvenShopfy.API.AssemblyInfo.cs
+++ InvenShopfy.API/obj/Debug/net8.0/InvenShopfy.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa84c2854cd12c01801405c4b8bc7dbdcee61fbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17fe97af1b03e631549ceeb9aaedfde2041ea8d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
