fa84c2854cd12c01801405c4b8bc7dbdcee61fbb
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-05 15:53:46 +0100

    adding changes

--- InvenShopfy.API/EndPoints/Endpoint.cs
+++ InvenShopfy.API/EndPoints/Endpoint.cs
@@ -1,4 +1,6 @@
 using InvenShopfy.API.Common.Api;
+using InvenShopfy.API.EndPoints.Brands;
+using InvenShopfy.API.EndPoints.Categories;
 using InvenShopfy.API.EndPoints.Products;
 
 namespace InvenShopfy.API.EndPoints;
@@ -13,19 +15,35 @@ public static class Endpoint
 
         endpoints.MapGroup("v2/products")
             .WithTags("Products")
-            .MapEndpoint<CreateProductEndpoint>();
+            .MapEndpoint<CreateProductEndpoint>()
+            .MapEndpoint<UpdateProductEndpoint>()
+            .MapEndpoint<GetAllProductsEndpoints>()
+            .MapEndpoint<DeleteProductEndpoint>()
+            .MapEndpoint<GetProductByIdEndpoint>();
         
         endpoints.MapGroup("v2/brands")
-            .WithTags("Brands");
-            // .MapEndpoint<CreateProductEndPoint>();
+            .WithTags("Brands")
+            .MapEndpoint<CreateBrandEndpoint>()
+            .MapEndpoint<UpdateBrandEndpoint>()
+            .MapEndpoint<GetAllBrandsEndpoint>()
+            .MapEndpoint<DeleteBrandEndpoint>()
+            .MapEndpoint<GetBrandByIdEndpoint>();
         
         endpoints.MapGroup("v2/units")
-            .WithTags("Products");
-            // .MapEndpoint<CreateProductEndPoint>();
+            .WithTags("Products")
+            .MapEndpoint<CreateProductEndpoint>()
+            .MapEndpoint<UpdateProductEndpoint>()
+            .MapEndpoint<GetAllProductsEndpoints>()
+            .MapEndpoint<DeleteProductEndpoint>()
+            .MapEndpoint<GetProductByIdEndpoint>();
         
         endpoints.MapGroup("v2/categories")
-            .WithTags("Categories");
-            // .MapEndpoint<CreateProductEndPoint>();
+            .WithTags("Categories")
+            .MapEndpoint<CreateCategoryEndpoint>()
+            .MapEndpoint<UpdateCategoryEndpoint>()
+            .MapEndpoint<GetAllCategoriesEndpoint>()
+            .MapEndpoint<DeleteCategoryEndpoint>()
+            .MapEndpoint<GetCategoryByIdEndpoint>();
         
         
     }
