94bceacb32525a185adf83b86eea8a45604a73cb
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-05 15:01:46 +0100

    adding products

--- InvenShopfy.API/Handlers/ProductHandler.cs
+++ InvenShopfy.API/Handlers/ProductHandler.cs
@@ -35,4 +35,102 @@ public class ProductHandler(AppDbContext context) : IProductHandler
             return new Response<Product?>(null, 500, "It was not possible to create a new Transaction");
         }
     }
+    public async Task<Response<Product?>> UpdateAsync(UpdateProductRequest request)
+    {
+        try
+        {
+            var product = await context.Products.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (product is null)
+            {
+                return new Response<Product?>(null, 404, "Product not found");
+            }
+            
+            product.Title = request.Title;
+            product.Price = request.Price;
+            product.ProductCode = request.ProductCode;
+            product.UnitId = request.UnitId;
+            product.BrandId = request.BrandId;
+            product.CategoryId = request.CategoryId;
+            product.ProductImage = request.ProductImage;
+            context.Products.Update(product);
+            await context.SaveChangesAsync();
+            return new Response<Product?>(product, message: "Product updated successfully");
+
+        }
+        catch 
+        {
+            return new Response<Product?>(null, 500, "It was not possible to update this Product");
+        }
+    }
+
+    public async Task<Response<Product?>> DeleteAsync(DeleteProductRequest request)
+    {
+        try
+        {
+            var product = await context.Products.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+            
+            if (product is null)
+            {
+                return new Response<Product?>(null, 404, "Product not found");
+            }
+
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+            return new Response<Product?>(product, message: "Product removed successfully");
+
+        }
+        catch 
+        {
+            return new Response<Product?>(null, 500, "It was not possible to delete this Product");
+        }
+    }
+    
+    public async Task<Response<Product?>> GetByIdAsync(GetProductByIdRequest request)
+    {
+        try
+        {
+            var product = await context.Products.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+            
+            if (product is null)
+            {
+                return new Response<Product?>(null, 404, "Product not found");
+            }
+
+            return new Response<Product?>(product);
+
+        }
+        catch 
+        {
+            return new Response<Product?>(null, 500, "It was not possible to find this Product");
+        }
+    }
+    public async Task<PagedResponse<List<Product>?>> GetByPeriodAsync(GetAllProductsRequest request)
+    {
+        try
+        {
+            var query = context
+                .Products
+                .AsNoTracking()
+                .Where(x => x.UserId == request.UserId)
+                .OrderBy(x => x.Title);
+            
+            var products = await query
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .ToListAsync();
+            
+            var count = await query.CountAsync();
+            
+            return new PagedResponse<List<Product>?>(
+                products,
+                count,
+                request.PageNumber,
+                request.PageSize);
+        }
+        catch 
+        {
+            return new PagedResponse<List<Product>?>(null, 500, "It was not possible to consult all Products");
+        }
+    }
 }
--- InvenShopfy.Core/Handlers/Product/IProductHandler.cs
+++ InvenShopfy.Core/Handlers/Product/IProductHandler.cs
@@ -6,8 +6,8 @@ namespace InvenShopfy.Core.Handlers.Product;
 public interface IProductHandler
 {
     Task<Response<Models.Product.Product?>> CreateAsync(CreateProductRequest request);
-    // Task<Response<Models.Product.Product?>> UpdateAsync(UpdateProductRequest request);
-    // Task<Response<Models.Product.Product?>> DeleteAsync(DeleteProductRequest request);
-    // Task<Response<Models.Product.Product?>> GetByIdAsync(GetProductByIdRequest request);
-    // Task<PagedResponse<List<Models.Product.Product>?>> GetByPeriodAsync(GetAllProductsRequest request);
+    Task<Response<Models.Product.Product?>> UpdateAsync(UpdateProductRequest request);
+    Task<Response<Models.Product.Product?>> DeleteAsync(DeleteProductRequest request);
+    Task<Response<Models.Product.Product?>> GetByIdAsync(GetProductByIdRequest request);
+    Task<PagedResponse<List<Models.Product.Product>?>> GetByPeriodAsync(GetAllProductsRequest request);
 }
