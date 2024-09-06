ff9df12d6461c8001d2ea2b5d3fd01fd2dac732d
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-06 17:18:58 +0100

    adding expenses mapping

---
+++ InvenShopfy.API/Data/Mapping/Expenses/ExpenseCategoryMapping.cs
@@ -0,0 +1,29 @@
+using InvenShopfy.Core.Models.Expenses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvenShopfy.API.Data.Mapping.Expenses;
+
+public class ExpenseCategoryMapping : IEntityTypeConfiguration<ExpenseCategory>
+{
+    public void Configure(EntityTypeBuilder<ExpenseCategory> builder)
+    {
+        builder.ToTable("ExpenseCategory");
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Category)
+            .IsRequired(true)
+            .HasColumnType("VARCHAR");
+        
+        builder.Property(x => x.SubCategory)
+            .IsRequired(false)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+        
+        builder.Property(x => x.UserId)
+            .IsRequired(true)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+    }
+    
+}
---
+++ InvenShopfy.API/Data/Mapping/Expenses/ExpenseMapping.cs
@@ -0,0 +1,55 @@
+using InvenShopfy.Core.Models.Expenses;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvenShopfy.API.Data.Mapping.Expenses;
+
+public class ExpenseMapping : IEntityTypeConfiguration<Expense>
+{
+    public void Configure(EntityTypeBuilder<Expense> builder)
+    {
+        builder.ToTable("Expense");
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Date)
+            .IsRequired(true)
+            .HasColumnType("TIMESTAMPTZ");
+        
+        builder.Property(x => x.WarehouseId)
+            .IsRequired(false)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+        
+        builder.Property(x => x.ExpenseType)
+            .IsRequired(true)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+        
+            
+        builder.Property(x => x.ExpenseCategoryId)
+            .IsRequired(true)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+        
+            
+        builder.Property(x => x.VoucherNumber)
+            .IsRequired(true)
+            .HasColumnType("NUMERIC(18,2)")
+            .HasMaxLength(160);
+
+        builder.Property(x => x.Amount)
+            .IsRequired(true)
+            .HasColumnType("NUMERIC(18,2)");
+        
+        builder.Property(x => x.PurchaceNote)
+            .IsRequired(true)
+            .HasColumnType("TEXT")
+            .HasMaxLength(500);
+        
+        builder.Property(x => x.UserId)
+            .IsRequired(true)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+    }
+}
