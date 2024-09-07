08f9c32e720b13b2e034c6ecc1210a1e3a8c74e0
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-07 14:41:10 +0100

    adding db contexts

--- InvenShopfy.API/Data/AppDbContext.cs
+++ InvenShopfy.API/Data/AppDbContext.cs
@@ -1,7 +1,10 @@
 using System.Reflection;
+using InvenShopfy.Core.Models.Expenses;
+using InvenShopfy.Core.Models.People;
 using InvenShopfy.Core.Models.Product;
+using InvenShopfy.Core.Models.UserManagement;
+using InvenShopfy.Core.Models.Warehouse;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace InvenShopfy.API.Data
 {
@@ -15,6 +18,19 @@ namespace InvenShopfy.API.Data
         public DbSet<Brand> Brands { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Unit> Unit { get; set; } = null!;
+        
+        public DbSet<ExpenseCategory> ExpenseCategories { get; set; } = null!;
+        public DbSet<Expense> Expenses { get; set; } = null!;
+        
+        public DbSet<Biller> Billers { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Supplier> Suppliers { get; set; } = null!;
+        
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        
+        public DbSet<Warehouse> Warehouses { get; set; } = null!;
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
