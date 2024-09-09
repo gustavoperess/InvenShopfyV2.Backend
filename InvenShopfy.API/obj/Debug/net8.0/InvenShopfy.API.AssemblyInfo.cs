e732cc3eaf8c99b1513523d630175110ec5d87f2
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-09 16:28:40 +0100

    adding Identity

--- InvenShopfy.API/obj/Debug/net8.0/InvenShopfy.API.AssemblyInfo.cs
+++ InvenShopfy.API/obj/Debug/net8.0/InvenShopfy.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9ed4d57f79b0761e6f544c0e7135760fc6c9bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4bb85e47749c0611f37635cd0821cd76d1f7bb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvenShopfy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
