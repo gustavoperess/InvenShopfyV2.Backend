33117d0fbbec4f16edcee04c6297710846fca9b0
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-11 15:31:44 +0100

    adding Sales

---
+++ InvenShopfy.Core/Enum/ESaleStatus.cs
@@ -0,0 +1,8 @@
+namespace InvenShopfy.Core.Enum;
+
+public enum ESaleStatus
+{
+    Complete = 1,
+    Incomplete = 2,
+    Drafts = 3
+}
--- InvenShopfy.Core/Models/Tradings/Sales/Sale.cs
+++ InvenShopfy.Core/Models/Tradings/Sales/Sale.cs
@@ -1,3 +1,4 @@
+using InvenShopfy.Core.Enum;
 using InvenShopfy.Core.Models.People;
 
 namespace InvenShopfy.Core.Models.Tradings.Sales;
@@ -5,6 +6,23 @@ namespace InvenShopfy.Core.Models.Tradings.Sales;
 public class Sale
 {
     public DateTime Date { get; set; } = DateTime.UtcNow;
-    public string Customer { get; set; }
-    public Customer CustomerId { get; set; }
+    public string Customer { get; set; } = String.Empty;
+    public Customer CustomerId { get; set; } = null!;
+    
+    public string Warehouse { get; set; } = String.Empty;
+    public Warehouse.Warehouse WarehouseId { get; set; } = null!;
+    
+    public string Biller { get; set; } = String.Empty;
+    public Biller BillerId { get; set; } = null!;
+    
+    public string Product { get; set; } = String.Empty;
+    public Product.Product ProductId { get; set; } = null!;
+
+    public int ShippingCost { get; set; }
+    public EPaymentStatus PaymentStatus { get; set; }
+    public ESaleStatus SaleStatus { get; set; }
+    public string Document { get; set; } = String.Empty;
+    public string SalesNote { get; set; } = String.Empty;
+    public string StafNote { get; set; } = String.Empty;
+    
 }
